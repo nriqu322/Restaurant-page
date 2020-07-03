4c2ff6ad34b96d6a591e74650c64369c05041038
Luis Saavedra <nriqu322@gmail.com>
2020-07-03 13:44:57 -0600

    add divs for menu

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,7 @@ const menuPage = () => {
   const content = document.querySelector('#content');
   content.classList.add('menu-page');
   const mainContent = document.createElement('div');
-  mainContent.id = 'main-content'
+  mainContent.id = 'main-content';
   mainContent.classList.add('main-content', 'menu-page');
   content.appendChild(mainContent);
 
@@ -15,6 +15,18 @@ const menuPage = () => {
   menuTitle.textContent = 'See our menu';
   
   menuInfo.appendChild(menuTitle);
+
+  const cardContainer = document.createElement('div');
+  cardContainer.classList.add('card-container', 'd-flex', 'row', 'col-12', 'col-md-6', 'col-lg-4');
+  menuInfo.appendChild(cardContainer);
+
+  for (let i = 0; i < 7; i += 1) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.id = `card-${i}`;
+    card.textContent = 'Text Here'
+    cardContainer.appendChild(card);
+  }
 }
 
 export default menuPage;
